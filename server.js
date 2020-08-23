a627f410b24319daf61577cea1445a2491da8826
Gabor Juhasz <gabor.juhasz@accretivetg.com>
2020-08-23 20:10:38 +0200

    wip

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+const WebSocket = require('ws')
+let wss
+let games = []
+
 const { createServer } = require('http')
 const next = require('next')
 
@@ -13,58 +17,55 @@ app.prepare().then(() => {
   }).listen(port, (err) => {
     if (err) throw err
     console.log(`> Ready on http://localhost:${port}`)
-  })
-})
 
-const WebSocket = require('ws')
-const wss = new WebSocket.Server({ port: 9898 })
+    wss = new WebSocket.Server({ port: 9898 })
 
-let games = []
+    wss.on('connection', function connection(ws) {
+      console.log('WS server started')
 
-wss.on('connection', function connection(ws) {
-  console.log('WS server started')
+      ws.on('message', function incoming(message) {
+        const data = JSON.parse(message)
 
-  ws.on('message', function incoming(message) {
-    const data = JSON.parse(message)
+        switch (data.type) {
+          case 'start':
+            const game = {
+              ts: new Date().getTime(),
+              players: [{ name: data.name }],
+            }
+            games.push(game)
 
-    switch (data.type) {
-      case 'start':
-        const game = {
-          ts: new Date().getTime(),
-          players: [{ name: data.name }],
+            ws.send(
+              JSON.stringify({
+                type: 'start',
+                game,
+              })
+            )
+            return
+
+          case 'join':
+            games = games.map((game) => {
+              if (game.ts !== data.ts) return
+
+              game.players.push({ name: data.name })
+              return game
+            })
+
+            ws.send(
+              JSON.stringify({
+                type: 'join',
+                game: games.find((game) => game.ts !== data.ts),
+              })
+            )
+            return
+
+          case 'move':
+            ws.send(JSON.stringify(data))
+            return
+
+          default:
+            throw new Error('Invalid data type.')
         }
-        games.push(game)
-
-        ws.send(
-          JSON.stringify({
-            type: 'start',
-            game,
-          })
-        )
-        return
-
-      case 'join':
-        games = games.map((game) => {
-          if (game.ts !== data.ts) return
-
-          game.players.push({ name: data.name })
-          return game
-        })
-
-        ws.send(
-          JSON.stringify({
-            type: 'join',
-            game: games.find((game) => game.ts !== data.ts),
-          })
-        )
-        return
-
-      case 'move':
-        ws.send(JSON.stringify(data))
-        return
-
-      default:
-        throw new Error('Invalid data type.')
-    }
+      })
+    })
   })
 })
